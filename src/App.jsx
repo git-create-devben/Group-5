bd5d1406d0297fdd344c7499f44617a9c555afb5
Seunoke0310 <triumphantoke7@gmail.com>
2026-06-13 15:51:28 +0100

    revert unwanted file changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 // import React from "react";
 import "./App.css";
-import MiddleSection from "./components/MiddleSection";
 
 function App() {
   return (
     <div className="App">
       <h1>Solar System Project Baseline Ready</h1>
-      <MiddleSection />
     </div>
   );
 }
